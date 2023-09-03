465d8922ed4c185cffac6622a02001ad985da7f0
codeCamper07 <dommaraju.bhargav007@gmail.com>
2023-09-03 13:24:42 +0530

    new

--- components/header.js
+++ components/header.js
@@ -1,9 +1,41 @@
+import { useState } from 'react'
 import style from '../styles/Home.module.css'
+import MenuTwoToneIcon from '@mui/icons-material/MenuTwoTone'
+import ClearTwoToneIcon from '@mui/icons-material/ClearTwoTone'
 
 const Header = () => {
+  const [menuActive, setmenuActive] = useState(false)
+
+  const menuToggle = () => {
+    setmenuActive(!menuActive)
+  }
   return (
     <div className={style.Headers}>
-      <img src='/ThomasMiller.png' alt='logo' className={style.logo} />
+      <div className={style.menu}>
+        <img src='/ThomasMiller.png' alt='logo' className={style.logo} />
+        <button
+          style={{ backgroundColor: 'rgb(20, 19, 25)', border: '0px' }}
+          onClick={menuToggle}>
+          {menuActive ? <ClearTwoToneIcon /> : <MenuTwoToneIcon />}
+        </button>
+      </div>
+      {menuActive ? (
+        <nav>
+          <ul style={{ listStyle: 'none', padding: '10px' }}>
+            <a href='#'>
+              <li>Home</li>
+            </a>
+            <a href='#aboutme'>
+              <li>About me</li>
+            </a>
+            <a href='#skills'>
+              <li>Skills</li>
+            </a>
+          </ul>
+        </nav>
+      ) : (
+        ''
+      )}
     </div>
   )
 }
